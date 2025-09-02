fc6096d0ab2574075381279273e0bdfd82e39abe
Parsa <12parsa@gmail.com>
2025-09-02 15:23:34 +0330

    chore: add more button

--- src/app/(public)/articles/(index)/loading.tsx
+++ src/app/(public)/articles/(index)/loading.tsx
@@ -1,7 +1,7 @@
 import {VerticalArticleCardSkeleton} from "@/features/home-page/components/article-card-vertical";
 
 function ArticlesLoading() {
-  return [1, 2, 3, 4, 5, 6].map((num) => {
+  return [1, 2, 3, 4].map((num) => {
     return <VerticalArticleCardSkeleton key={num} />;
   });
 }
