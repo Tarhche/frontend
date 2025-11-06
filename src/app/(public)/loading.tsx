cfcfe47b2351b7a97da25504a06f36333400d29c
parsa <parsa_gholipour@yahoo.com>
2025-11-06 13:47:06 +0330

    chore: add loading page to home page

---
+++ src/app/(public)/loading.tsx
@@ -0,0 +1,5 @@
+import {FeaturedArticlesSkeleton} from "@/features/home-page/components/featured-articles";
+
+export default async function HomePageLoading() {
+  return <FeaturedArticlesSkeleton />;
+}
--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,8 +1,6 @@
 import {type Metadata} from "next";
-import {ReactNode, Suspense} from "react";
 import {
   FeaturedArticles,
-  FeaturedArticlesSkeleton,
 } from "@/features/home-page/components/featured-articles";
 import {fetchHomePageData} from "@/dal/public/home";
 import Element from "@/features/elements/element";
@@ -15,10 +13,10 @@ export default async function HomePage() {
   const homePageData = await fetchHomePageData();
 
   return (
-    <Suspense fallback={<FeaturedArticlesSkeleton /> as ReactNode}>
+    <>
       <Element style={{ marginTop: '1rem' }} type={'jumbotron'} elements={homePageData.elements} />
       <Element style={{ marginTop: '1rem' }} type={'featured'} elements={homePageData.elements} />
       <FeaturedArticles latestArticles={homePageData.all} popularArticles={homePageData.popular} />
-    </Suspense>
+    </>
   );
 }
