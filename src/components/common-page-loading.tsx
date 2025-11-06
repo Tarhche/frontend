038e1ecb9a35acabecf26d2cbe18fcc419fc8b20
parsa <parsa_gholipour@yahoo.com>
2025-11-06 10:46:53 +0330

    chore: add loading for navigates

---
+++ src/components/common-page-loading.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import {Center, Stack, Loader, Paper} from "@mantine/core";
+
+function CommonPageLoading({fullScreen = true}: {fullScreen?: boolean}) {
+  return (
+    <Center
+      style={
+        fullScreen
+          ? {minHeight: "100vh", width: "100%"}
+          : {padding: "2rem", width: "100%"}
+      }
+    >
+      <Paper radius="lg" p="xl" withBorder shadow="sm">
+        <Stack align="center" gap="xs">
+          <Loader size="lg" />
+        </Stack>
+      </Paper>
+    </Center>
+  );
+}
+
+export default CommonPageLoading;
