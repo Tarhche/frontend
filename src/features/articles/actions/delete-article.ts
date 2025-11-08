13b0dc8fd7beadb8cff63772ce4b08b86ddf2fd6
Mahdi Khanzadi <khanzadimahdi@gmail.com>
2025-11-08 14:19:59 +0100

    upgrade to NodeJS 24.10 LTS

--- src/features/articles/actions/delete-article.ts
+++ src/features/articles/actions/delete-article.ts
@@ -1,4 +1,5 @@
 "use server";
+
 import {revalidatePath} from "next/cache";
 import {APP_PATHS} from "@/lib/app-paths";
 import {privateDalDriver} from "@/dal/private/private-dal-driver";
--- src/features/files/actions/delete-file.ts
+++ src/features/files/actions/delete-file.ts
@@ -1,3 +1,5 @@
+"use server";
+
 import {revalidatePath} from "next/cache";
 import {APP_PATHS} from "@/lib/app-paths";
 import {deleteFile} from "@/dal/private/files";
